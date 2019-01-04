9ec1d700155a007702bb1c35c67ba518b4cbd009
Andrew <awolfe76@gmail.com>
2019-01-04 09:10:18 -0500

    better authentication and authorization handling; break out a few components

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = props => {
+  return (
+    <header className="grid">
+      <h1 className="App-title">HMDA Help</h1>
+      <nav>
+        <Link to="/">Search</Link>
+        <Link
+          to={{
+            pathname: '/add',
+            state: {
+              institution: {
+                lei: '',
+                taxId: '',
+                respondentName: ''
+              }
+            },
+            token: props.token
+          }}
+        >
+          Add a new institution
+        </Link>
+        <button onClick={() => props.logout()}>Logout</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
