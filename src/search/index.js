d5b97da33e7b526bfb633f64898007da86346b41
Andrew <awolfe76@gmail.com>
2019-01-25 15:24:11 -0500

    use system fonts

--- src/search/index.js
+++ src/search/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-import './Form.css'
 import '../Loading.css'
 
 import { searchInputs } from '../constants/inputs.js'
