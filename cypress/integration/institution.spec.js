c2e697257ff96113940351efc8547aa5fb9c67eb
Meissa Dia <meissadia@gmail.com>
2020-07-22 15:48:32 -0600

    Login to Keycloak without UI

--- cypress/integration/institution.spec.js
+++ cypress/integration/institution.spec.js
@@ -1,19 +1,36 @@
-let { HH_HOST, HH_USERNAME, HH_PASSWORD, HH_INSTITUTION } = Cypress.env()
+const {
+  HH_HOST,
+  HH_USERNAME,
+  HH_PASSWORD,
+  HH_INSTITUTION,
+  HH_AUTH_URL,
+  HH_AUTH_REALM,
+  HH_AUTH_CLIENT_ID
+} = Cypress.env()
 
 describe('Institution', () => {
-  it('Can update existing', () => {
-    // Debug Env
-    cy.log(HH_HOST)
-    cy.log(HH_USERNAME)
-    cy.log(HH_PASSWORD)
-    cy.log(HH_INSTITUTION)
+  beforeEach(() => {
+    cy.login({
+      root: HH_AUTH_URL,
+      realm: HH_AUTH_REALM,
+      client_id: HH_AUTH_CLIENT_ID,
+      redirect_uri: HH_HOST,
+      username: HH_USERNAME,
+      password: HH_PASSWORD
+    })
+  })
 
-    // Log In
+  afterEach(() => {
+    cy.logout({
+      root: HH_AUTH_URL,
+      realm: HH_AUTH_REALM,
+      redirect_uri: HH_HOST
+    })
+  })
+
+  it('Can update existing', () => {
     cy.viewport(1600, 900)
     cy.visit(HH_HOST)
-    cy.findByLabelText('Email').type(HH_USERNAME)
-    cy.findByLabelText('Password').type(HH_PASSWORD, { log: false })
-    cy.findByText('Log In').click()
 
     // Search for existing Instititution
     /* TODO: Search by Label instead of ID once those changes are live */
